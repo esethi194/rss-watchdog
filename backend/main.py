fb7bc478dd4fb3e2762997387207e6b5d85d47b7
esethi194 <142650417+esethi194@users.noreply.github.com>
2026-05-23 20:04:10 -0700

    add GET, DELETE /monitors/{id} endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from database import create_tables, SessionLocal, Monitor
 from schemas import MonitorCreate, MonitorResponse
 
@@ -29,3 +29,23 @@ def create_monitors(monitor: MonitorCreate):
     db.refresh(new_monitor)
     db.close()
     return new_monitor
+
+@app.get("/monitors/{id}", response_model=MonitorResponse)
+def get_monitors(id: int):
+    db = SessionLocal()
+    monitors = db.query(Monitor).filter(Monitor.id == id).first()
+    if not monitors:
+        raise HTTPException(status_code=404, detail="Monitor not found")
+    db.close()
+    return monitors
+
+@app.delete("/monitors/{id}")
+def delete_monitors(id: int):
+    db = SessionLocal()
+    monitors = db.query(Monitor).filter(Monitor.id == id).first()
+    if not monitors:
+        raise HTTPException(status_code=404, detail="Monitor not found")
+    db.delete(monitors)
+    db.commit()
+    db.close()
+    return {"message": f"Monitor {id} deleted"}
